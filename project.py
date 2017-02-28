784dba98a6f0d9b9c2024e02b5535bef6afc5ebd
RAMSRIG\Ramsri <ramsrigouthamg@gmail.com>
2017-02-27 22:17:20 -0800

    Camera Calibration Working

---
+++ project.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import glob
+
+
+def getCalibration():
+    images = glob.glob('camera_cal/calibration*.jpg')
+    objpoints = []
+    imgpoints=[]
+    nrows= 6
+    ncols = 9
+    objp = np.zeros((nrows*ncols,3),np.float32)
+    objp[:,:2] = np.mgrid[0:ncols,0:nrows].T.reshape(-1,2)
+    counter = 0
+    for fname in images:
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        ret,corners = cv2.findChessboardCorners(gray,(ncols,nrows),None)
+        if ret:
+            counter = counter + 1
+            imgpoints.append(corners)
+            objpoints.append(objp)
+            # img = cv2.drawChessboardCorners(img,(ncols,nrows),corners,ret)
+            # cv2.imshow("temp",img)
+
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    testImage = cv2.imread(images[0])
+    dst = cv2.undistort(testImage, mtx, dist, None, mtx)
+    cv2.imwrite("output_images/undistorted_1.jpg", dst)
+    # cv2.imshow("temp",dst)
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    getCalibration()
