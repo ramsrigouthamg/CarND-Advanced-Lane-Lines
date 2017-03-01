eb8f90abf8aec2c973fa2e21a9755da96bf80cb2
RAMSRIG\Ramsri <ramsrigouthamg@gmail.com>
2017-03-01 07:23:28 -0800

    Working track bars.

--- project.py
+++ project.py
@@ -31,23 +31,68 @@ def getCalibration():
     return mtx,dist
 
 
+def plot_images(original,img1,img2,img3,orig_label,label1,label2,label3):
+    b, g, r = cv2.split(original)  # get b,g,r
+    rgb_img = cv2.merge([r, g, b])
+    plt.subplot(2, 2, 1)
+    plt.imshow(rgb_img)
+    plt.xlabel(orig_label)
+
+    plt.subplot(2, 2, 2)
+    plt.imshow(img1,cmap='gray')
+    plt.xlabel(label1)
+    plt.subplot(2, 2, 3)
+    plt.imshow(img2,cmap='gray')
+    plt.xlabel(label2)
+    plt.subplot(2, 2, 4)
+    plt.imshow(img3,cmap='gray')
+    plt.xlabel(label3)
+    plt.show()
 
 
-
-
-
+def nothing(x):
+    pass
 
 
 
 if __name__ == "__main__":
-    mtx_matrix , dist_matrix = getCalibration()
-    testImage1 = cv2.imread("camera_cal/calibration1.jpg")
-    dst1 = cv2.undistort(testImage1, mtx_matrix, dist_matrix, None, mtx_matrix)
-    cv2.imwrite("output_images/undistorted_1.jpg", dst1)
+    # mtx_matrix , dist_matrix = getCalibration()
+    # testImage1 = cv2.imread("camera_cal/calibration1.jpg")
+    # dst1 = cv2.undistort(testImage1, mtx_matrix, dist_matrix, None, mtx_matrix)
+    # cv2.imwrite("output_images/undistorted_1.jpg", dst1)
+    #
+    # testImage2 = cv2.imread("test_images/straight_lines1.jpg")
+    # dst2 = cv2.undistort(testImage2, mtx_matrix, dist_matrix, None, mtx_matrix)
+    # cv2.imwrite("output_images/straight_lines1_output.jpg", dst2)
 
-    testImage2 = cv2.imread("test_images/straight_lines1.jpg")
-    dst2 = cv2.undistort(testImage2, mtx_matrix, dist_matrix, None, mtx_matrix)
-    # cv2.imshow("temp",dst2)
-    cv2.imwrite("output_images/straight_lines1_output.jpg", dst2)
-    print("Hello")
+    testImage = cv2.imread("test_images/test5.jpg")
+    # cv2.imshow('test',testImage)
+    hls = cv2.cvtColor(testImage, cv2.COLOR_BGR2HLS)
+    H = hls[:, :, 0]
+    L = hls[:, :, 1]
+    S = hls[:, :, 2]
+    thresh_S = (100,255)
+    binary_S = np.zeros_like(S)
+    binary_S[(S > thresh_S[0]) & (S <= thresh_S[1])] = 1
+    # plot_images(testImage, H, binary_S, S, 'original', 'H', 'binary_S', 'S')
+    cv2.namedWindow('image_S')
+    cv2.createTrackbar('S_lower', 'image_S', 0, 255, nothing)
+    cv2.createTrackbar('S_higher', 'image_S', 0, 255, nothing)
+    temp = np.zeros_like(S)
+
+    while (1):
+
+        cv2.imshow('image_S', temp)
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+        temp = np.zeros_like(S)
+        s_low = cv2.getTrackbarPos('S_lower', 'image_S')
+        s_high = cv2.getTrackbarPos('S_higher', 'image_S')
+        temp[(S > s_low) & (S <= s_high)] = 255
+
+    cv2.destroyAllWindows()
+
+
+
 
